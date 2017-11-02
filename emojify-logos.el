632aba52ce07270dc42c4e98e015451ba2c9bd27
mxgoldstein <mxgoldstein@users.noreply.github.com>
2017-11-02 02:30:35 +0100

    Make emojify-logos.el a valid package

--- emojify-logos.el
+++ emojify-logos.el
@@ -1,24 +1,30 @@
-;; emojify-logos.el --- Add logos to emojify
+;;; emojify-logos.el --- Add logos to emojify
 
 ;; Copyright (C) 2017 mxgoldstein
+
+;; This script is licensed under 
 
 ;; Author: mxgoldstein <m_goldstein@gmx.net>
 ;; URL: https://github.com/mxgoldstein/emojify-logos
 ;; Version: 0.1
 ;; Package-Requires: ((emojify "0.4"))
 
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
 ;;; Commentary:
 
-;; This package adds icons for various programming languages and tools to emojify.el
+;; This package adds logo icons for various programming languages and tools to emojify.el
 ;; All icons are the property of their respective owners and may be trademarked and/or restricted in the way they may be used. See COPYRIGHT.MD for more details.
-
 
 ;;; TODO
 
 ;; Add more logos for languages / modes etc.
-;; Use uniform icon sizes (128x128)
+;; Use uniform icon sizes
 ;; Replace low-quality C icon
-;; Find a (somewhat free) icon for java
+;; Find a (somewhat) free icon for java
 
 (require 'emojify)
 (require 'url)
@@ -49,3 +55,7 @@
 (emojify-set-emoji-data)
 
 (provide 'emojify-logos)
+
+;;; emojify-logos.el ends here
+
+
